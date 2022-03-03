4aa0e7f4a49ab40759cdda84591fcaff99467a9f
Amir Bhattarai <alphaelite10@gmail.com>
2022-03-03 10:23:44 +0545

    Html and Css

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -10,16 +10,16 @@ export default function(){
     
         <Header />
 
-        <div className='app__signup-box'>
-            <h1>Sign Up</h1>
+        <form className='app__signup-box'>
+            <h1 className='app__sign-up'>Sign Up</h1>
             <Formfield labeltitle='Name' fieldtype={Text} />          
             <Formfield labeltitle='Email Address' fieldtype='email' />          
             <Formfield labeltitle='Password' fieldtype='password' />          
             <Formfield labeltitle='Re-type Password' fieldtype='password' />
-            <p>Already have an account? Login Here</p>     
-            <button value={'Sign Up'}></button>     
+            <p className='app__login-link'>Already have an account? Login Here</p>     
+            <button className='app__signup-btn'>Sign Up</button>     
 
-        </div>
+        </form>
 
 
         <Footer />
