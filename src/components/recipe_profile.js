e7b30c2a2535d5dfe3b41655d5bedb3cc68f4c7e
Sankalpa <snksatyal@gmail.com>
2022-03-23 18:06:57 +0545

    Update recipe_profile.js

--- src/components/recipe_profile.js
+++ src/components/recipe_profile.js
@@ -4,7 +4,7 @@ export default function recipe_profile(props){
 
     return(
     <div  className="app__recipe_profile">
-        <img className='app__webview-show' src={props.img} alt='none'/>
+        <img className='app__recipeImg' src={props.img} alt='none'/>
         <h2 className="app__image_title">{props.title}</h2>
     </div>
     );
