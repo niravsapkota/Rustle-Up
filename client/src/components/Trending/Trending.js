8e6dd081d489870aee5040d42816167a0c5a0e92
Nirav Sapkota <67631766+niravsapkota@users.noreply.github.com>
2022-03-30 11:04:20 +0545

    structured folders

---
+++ client/src/components/Trending/Trending.js
@@ -0,0 +1,29 @@
+import React from "react";
+import TrendingTile from "./TrendingTile";
+import pic from "../../assets/unsplash_8T9AVksyt7s.png";
+
+export default function() {
+  return (
+    <>
+      <p className="app__trending-title">Trending</p>
+      <div className="app__trending-container">
+        <TrendingTile
+          img={pic}
+          title="Samosa"
+          description="A very well known snacks in south east asia. Its shell is made of flour and inside is the vegetables chopped off and served with sauces"
+        />
+        <TrendingTile
+          img={pic}
+          title="Samosa"
+          description="A very well known snacks in south east asia. Its shell is made of flour and inside is the vegetables chopped off and served with sauces"
+        />
+        <TrendingTile
+          img={pic}
+          title="Samosa"
+          description="A very well known snacks in south east asia. Its shell is made of flour and inside is the vegetables chopped off and served with sauces"
+        />
+        <p className="app__trending-next">Next</p>
+      </div>
+    </>
+  );
+}
