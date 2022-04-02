f15b4f33d6df46f479572ff23b6d659b5c610d26
Sankalpa <snksatyal@gmail.com>
2022-04-02 09:04:19 +0545

    Established connection w/ mongodb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json(
+    {limit:"50mb", extended:true}
+));
+
+app.use(bodyParser.urlencoded(
+    {limit:"50mb", extended:true}
+));
+
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://admin:admin@cluster0.4qhg3.mongodb.net/rustleup?retryWrites=true&w=majority";
+const PORT=process.env.PORT || 5000;
+ 
+mongoose.connect(CONNECTION_URL)
+    .then(
+        ()=>app.listen(
+            PORT,
+        ()=>console.log(`Server Running on port : ${PORT}`)
+        ))
+    .catch((error)=>console.log(error.message));
+
+// mongoose.set("useFindAndModify",false);
+
