80f70c13fe276c161d937c5bd927577bf1ff929b
Nirav Sapkota <67631766+niravsapkota@users.noreply.github.com>
2022-04-22 21:50:40 +0545

    fixed merge conflict

--- client/src/components/Recipe/Recipe.js
+++ client/src/components/Recipe/Recipe.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { FcEmptyTrash, FcPrint } from "react-icons/fc";
-import { HiPencil } from "react-icons/hi"
+import { HiPencil } from "react-icons/hi";
 import pic from "../../assets/unsplash_8T9AVksyt7s.png";
 import RecipeProfile from "./recipe_profile";
 import RecipeDetails from "./recipe_details";
@@ -10,15 +10,14 @@ import FormField from "../Auth/Formfield";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import createReview from "../CreateReview";
 
-
 export default function Recipe() {
   const navigate = useNavigate();
 
   // initial blank state of form
   const [reviewDetails, setReview] = useState({
-    review : "",
+    review: "",
     userID: "",
-    recipeID: ""
+    recipeID: "",
   });
 
   // function to dynamically update fields
@@ -34,17 +33,17 @@ export default function Recipe() {
   const btnSubmit = async (e) => {
     e.preventDefault();
     axios
-    .post("/recipe/create-review", reviewDetails)
-    .then((response) => {
-          window.alert("Posted.");
-          navigate("/recipe");
-    })
-    .catch((error) => {
-            window.alert("Something went wrong");
-    });
-  }
+      .post("/recipe/create-review", reviewDetails)
+      .then((response) => {
+        window.alert("Posted.");
+        navigate("/recipe");
+      })
+      .catch((error) => {
+        window.alert("Something went wrong");
+      });
+  };
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   let { id } = useParams();
 
   const getRecipe = async () => {
@@ -64,59 +63,65 @@ export default function Recipe() {
     } catch (error) {
       console.log("Error Caught!");
     }
-
-  }
+  };
 
   useEffect(() => {
     getRecipe();
   }, []);
 
-
   return (
     <>
       <div className="app__recipe">
         <div className="app__recipebody">
-          
           <div className="app__recipeProfile">
-            <RecipeProfile img={pic} title={data.title} />
+            <RecipeProfile img={data.image_url} title={data.title} />
           </div>
 
           <div className="app__recipedetails">
-            <FcPrint size={35}/>
+            <FcPrint size={35} />
 
-            <HiPencil size={35} onClick={
-              () => axios.get(`/recipe/get/${id}`).
-                then(
-                  navigate(`/create-recipe/${id}`)
-                )} />
-          
-            <FcEmptyTrash size={35} onClick={
-              () => axios.delete(`/recipe/delete/${id}`).
-                then(
-                  navigate("/profile-my-recipe")
-                )} />
+            <HiPencil
+              size={35}
+              onClick={() =>
+                axios
+                  .get(`/recipe/get/${id}`)
+                  .then(navigate(`/create-recipe/${id}`))
+              }
+            />
+
+            <FcEmptyTrash
+              size={35}
+              onClick={() =>
+                axios
+                  .delete(`/recipe/delete/${id}`)
+                  .then(navigate("/profile-my-recipe"))
+              }
+            />
             <RecipeDetails data={data} />
           </div>
-
         </div>
-{/**/}
+        {/**/}
         <div className="app__recipeReview">
-
           <h1 className="app__recipeReview_header">Reviews</h1>
           <form className="app__create-box2">
-            
-            <FormField 
+            <FormField
               labeltitle="Write Review :"
               name="review"
               fieldtype={Text}
               onChange={handleChange}
             />
-          <button className="app__create-btn" type="submit" onClick={btnSubmit}> Add Review </button>
-        </form>
+            <button
+              className="app__create-btn"
+              type="submit"
+              onClick={btnSubmit}
+            >
+              {" "}
+              Add Review{" "}
+            </button>
+          </form>
 
           <hr></hr>
           <RecipeReview />
-
         </div>
       </div>
     </>
