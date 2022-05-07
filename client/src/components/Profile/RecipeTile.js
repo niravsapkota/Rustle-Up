3d88b1aa542771f93c3c00807f9e2ebe4d72b8c8
Sankalpa <snksatyal@gmail.com>
2022-05-07 11:02:06 +0545

    Update RecipeTile.js

--- client/src/components/Profile/RecipeTile.js
+++ client/src/components/Profile/RecipeTile.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function RecipeTile(props) {
+  const navigate = useNavigate();
+
   return (
-    <div className="app__recipe-tile">
-      <img className="recipe-img" src={props.img} alt="none" />
+    <div 
+      className="app__recipe-tile"
+      onClick={() =>
+        axios
+          .get(`/recipe/get/${props.element._id}`)
+          .then(navigate(`/recipe/${props.element._id}`))
+        }
+      >
+      <img className="recipe-img" src={props.element.image_url} alt="none" />
       <div className="recipe-text-container">
-        <h3 className="recipe-img-title">{props.title}</h3>
-        <p className="recipe-description">{props.description}</p>
+        <h3 className="recipe-img-title">{props.element.title}</h3>
+        <p className="recipe-description">{props.element.description}</p>
       </div>
     </div>
   );
