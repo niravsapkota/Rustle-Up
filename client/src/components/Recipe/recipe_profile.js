88a28f3a4aa5a99f2044ffa48812f44a4f025f67
Sankalpa <snksatyal@gmail.com>
2022-05-07 12:57:01 +0545

    Update recipe_profile.js

--- client/src/components/Recipe/recipe_profile.js
+++ client/src/components/Recipe/recipe_profile.js
@@ -1,22 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function RecipeProfile(props) {
-  function isFav(x) {
-    if (x == 0) {
-      return false;
-    } else {
-      return true;
-    }
+  let {id} = useParams();
+
+  function isFav(x){
+    if(x==0){return false}
+    else{return true}
   }
 
-  let isnotfav = !isFav(props.fav);
+  let isfav = isFav(props.fav);
+
+  const btnAddToFav = async(e)=> {
+    e.preventDefault();
+    axios
+      .post(`/recipe/addToFav/${id}`)
+      .then((response) => {
+        window.alert("Added to Favourites!");
+      })
+      .catch((error) => window.alert("Something went wrong!"));
+  };
+  
+  const btnDelFromFav = async(e)=> {
+    e.preventDefault();
+    axios
+      .post(`/recipe/delfromfav/`)
+      .then((response) => {
+        window.alert("Removed from Favourites!");
+      })
+      .catch((error) => window.alert("Something went wrong!"));
+  };
 
   return (
     <div className="app__recipe_profile">
       <img className="app__recipeImg" src={props.img} alt="none" />
       <span className="app__profile-user-card-name">{props.title}</span>
-      <p className="app__recipe_profile-details">Creator: {props.creator}</p>
+      <p className="app__recipe_profile-details">
+        Creator: {props.creator}
+      </p>
       <p className="app__recipe_profile-details">
         Difficulty: {props.difficulty}
       </p>
@@ -24,13 +46,32 @@ export default function RecipeProfile(props) {
         Preparation time: {props.prep_time}
       </p>
       <p className="app__profile-user-card-options">
-        {props.logged ? (
+        {props.logged? (
           <p>
-            {isnotfav ? <btn>Delete from Favs</btn> : <btn>Add to Favs</btn>}
+            {isfav?(
+              <btn
+                onClick={btnDelFromFav}
+              >
+                Delete from Favs
+              </btn>
+            ):(
+              <btn
+                onClick={btnAddToFav}
+              >
+                Add to Favs
+              </btn>
+              )
+            }
           </p>
-        ) : (
-          <Link to="/login">Log In to Favourite</Link>
-        )}
+        ):(
+            <Link
+              style={{ color: "#7e7a05", textDecoration: "inherit" }}
+              to="/login"
+            >
+              Log In to Favourite
+            </Link>
+          )
+        }
       </p>
     </div>
   );
