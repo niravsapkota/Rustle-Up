a71b56fe45cc52f082049aeff6ff371dfe9b36cb
Sankalpa <snksatyal@gmail.com>
2022-05-07 13:02:26 +0545

    Revert "Update recipe_profile.js"
    This reverts commit 88a28f3a4aa5a99f2044ffa48812f44a4f025f67.

--- client/src/components/Recipe/recipe_profile.js
+++ client/src/components/Recipe/recipe_profile.js
@@ -1,44 +1,22 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
-import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function RecipeProfile(props) {
-  let {id} = useParams();
-
-  function isFav(x){
-    if(x==0){return false}
-    else{return true}
+  function isFav(x) {
+    if (x == 0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
-  let isfav = isFav(props.fav);
-
-  const btnAddToFav = async(e)=> {
-    e.preventDefault();
-    axios
-      .post(`/recipe/addToFav/${id}`)
-      .then((response) => {
-        window.alert("Added to Favourites!");
-      })
-      .catch((error) => window.alert("Something went wrong!"));
-  };
-  
-  const btnDelFromFav = async(e)=> {
-    e.preventDefault();
-    axios
-      .post(`/recipe/delfromfav/`)
-      .then((response) => {
-        window.alert("Removed from Favourites!");
-      })
-      .catch((error) => window.alert("Something went wrong!"));
-  };
+  let isnotfav = !isFav(props.fav);
 
   return (
     <div className="app__recipe_profile">
       <img className="app__recipeImg" src={props.img} alt="none" />
       <span className="app__profile-user-card-name">{props.title}</span>
-      <p className="app__recipe_profile-details">
-        Creator: {props.creator}
-      </p>
+      <p className="app__recipe_profile-details">Creator: {props.creator}</p>
       <p className="app__recipe_profile-details">
         Difficulty: {props.difficulty}
       </p>
@@ -46,32 +24,13 @@ export default function RecipeProfile(props) {
         Preparation time: {props.prep_time}
       </p>
       <p className="app__profile-user-card-options">
-        {props.logged? (
+        {props.logged ? (
           <p>
-            {isfav?(
-              <btn
-                onClick={btnDelFromFav}
-              >
-                Delete from Favs
-              </btn>
-            ):(
-              <btn
-                onClick={btnAddToFav}
-              >
-                Add to Favs
-              </btn>
-              )
-            }
+            {isnotfav ? <btn>Delete from Favs</btn> : <btn>Add to Favs</btn>}
           </p>
-        ):(
-            <Link
-              style={{ color: "#7e7a05", textDecoration: "inherit" }}
-              to="/login"
-            >
-              Log In to Favourite
-            </Link>
-          )
-        }
+        ) : (
+          <Link to="/login">Log In to Favourite</Link>
+        )}
       </p>
     </div>
   );
